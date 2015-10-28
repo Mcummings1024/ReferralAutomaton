587ccb66cdffea2299c6bbe9cffd16310b9496eb
Matthew <Mattxsc@gmail.com>
2015-10-28 13:37:45 -0400

    Got the table to update with new referrals

--- refCtrl.js
+++ refCtrl.js
@@ -4,16 +4,19 @@ var $jq = jQuery.noConflict();
 app.controller('RefCtrl', ['refService', function(refService) {
 	//code for adding/editing referrals goes here.
 	var self = this;
-	//var refs = refService.getRefs();
-	var table = document.getElementById('refTable');
+	self.refs = [
+				  {ref: "bologna", clicks: 132}, 
+				  {ref: "ham", clicks: 90}
+				];
 
 	//adds referral to the table and creates a splash page for it.
 	self.addReferral = function() {
-		console.log('test');
-		//refs.push({ref: newRef, clicks: 0});
-		var newDiv = document.createElement("div"); 
-  		var newContent = document.createTextNode("Hi there and greetings!"); 
-  		newDiv.appendChild(newContent);
+		console.log(self.refs[0]);
+		self.refs.push({ref: self.newRef, clicks: 0});
+	};
+
+	self.updateClicks = function(i) {
+		self.refs[i].clicks++;
 	};
 
 }]);
--- refService.js
+++ refService.js
@@ -2,11 +2,19 @@ var app = angular.module('refApp');
 
 app.factory('refService', function() {
 
-	var refs = [];
+	var refs = [
+				{ref: "bologna", clicks: 132}, 
+				{ref: "ham", clicks: 90}
+			   ];
 
 	return {
 		getRefs : function() {
 			return refs;
+		},
+
+		addRefs: function(ref) {
+			console.log('adding ref');
+			refs.push(ref);
 		},
 
 		refs: refs
